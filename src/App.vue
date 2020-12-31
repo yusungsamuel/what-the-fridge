3f9eb3d415eb0c17fc16149790d8d7c29637c469
yusungsamuel <ssyyu@ucdavis.edu>
2020-12-30 19:06:15 -0800

    create jumbotron component and removed not needed boilerplate file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Jumbotron/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Jumbotron from "./components/Jumbotron"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Jumbotron,
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap');
+  *{
+    font-family: 'Quicksand', sans-serif;
+  }
 </style>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>What The Fridge?!</h1>
+    <p>Find the perfect recipe for what you have in your fridge</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jumbotron",
+};
+</script>
+
+<style scoped>
+div {
+  width: 100%;
+  background-image: linear-gradient(rgb(255, 94, 0), rgb(255, 174, 0));
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1,p {
+  color: white;
+  text-align: center;
+}
+
+h1{
+    font-size: 100px;
+}
+</style>
